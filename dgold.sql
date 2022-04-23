4924c324b60bf4219746ac7420c5406808a92eaa
muhammedrahil <mrahil7510@gmail.com>
2022-04-23 20:22:15 +0530

    final before sql  product

--- dgold.sql
+++ dgold.sql
@@ -1,5 +1,5 @@
 /*
-SQLyog Community v13.1.2 (64 bit)
+SQLyog Community v13.0.1 (64 bit)
 MySQL - 5.5.20-log : Database - dgold
 *********************************************************************
 */
@@ -27,12 +27,14 @@ CREATE TABLE `employee` (
   `branch` varchar(50) DEFAULT NULL,
   `loginid` int(10) DEFAULT NULL,
   PRIMARY KEY (`emp_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `employee` */
 
 insert  into `employee`(`emp_id`,`emp_name`,`e-code`,`branch`,`loginid`) values 
-(1,'asd','12','1',2);
+(1,'Rahil','1234','malapuram',12),
+(2,'lalu','234','malapuram',13),
+(3,'asdfghh','123','malapuram',14);
 
 /*Table structure for table `leaverequest` */
 
@@ -42,19 +44,20 @@ CREATE TABLE `leaverequest` (
   `LRid` int(10) NOT NULL AUTO_INCREMENT,
   `reason` varchar(100) DEFAULT NULL,
   `status` varchar(50) DEFAULT NULL,
-  `emp_id` int(10) DEFAULT NULL,
+  `lg_id` int(10) DEFAULT NULL,
   `date` date DEFAULT NULL,
   `expDate` date DEFAULT NULL,
   PRIMARY KEY (`LRid`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 /*Data for the table `leaverequest` */
 
-insert  into `leaverequest`(`LRid`,`reason`,`status`,`emp_id`,`date`,`expDate`) values 
-(1,'medical','accept',1,'2022-03-25',NULL),
-(2,'medical','pending',1,NULL,'2022-03-08'),
-(3,'medical','pending',1,'2022-03-23',NULL),
-(4,'medical','accept',1,'2022-03-16','2022-03-23');
+insert  into `leaverequest`(`LRid`,`reason`,`status`,`lg_id`,`date`,`expDate`) values 
+(1,'medical','pending',12,'2022-04-26','2022-04-30'),
+(3,'medical','pending',12,'2022-04-14','2022-04-21'),
+(4,'medical','pending',13,'2022-04-28','2022-04-07'),
+(5,'marraige','pending',12,'2022-04-21','2022-04-20'),
+(6,'medical','accept',12,'2022-04-15','2022-04-13');
 
 /*Table structure for table `login` */
 
@@ -66,13 +69,35 @@ CREATE TABLE `login` (
   `ecode` varchar(50) DEFAULT NULL,
   `usertype` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`loginid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 /*Data for the table `login` */
 
 insert  into `login`(`loginid`,`username`,`ecode`,`usertype`) values 
-(1,'asd','123','admin'),
-(2,'asd','12','employee');
+(1,'admin','123','admin'),
+(5,'maneger','123','manager'),
+(12,'Rahil','1234','employee'),
+(13,'lalu','234','employee'),
+(14,'asdfghh','123','employee');
+
+/*Table structure for table `pyment` */
+
+DROP TABLE IF EXISTS `pyment`;
+
+CREATE TABLE `pyment` (
+  `pid` int(50) NOT NULL AUTO_INCREMENT,
+  `catogary` varchar(100) DEFAULT NULL,
+  `moneny` varchar(100) DEFAULT NULL,
+  `lg_id` int(10) DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`pid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `pyment` */
+
+insert  into `pyment`(`pid`,`catogary`,`moneny`,`lg_id`,`status`) values 
+(2,'TA','456',12,'accept'),
+(3,'TA','1000',12,'accept');
 
 /*Table structure for table `target` */
 
@@ -81,14 +106,17 @@ DROP TABLE IF EXISTS `target`;
 CREATE TABLE `target` (
   `trid` int(10) NOT NULL AUTO_INCREMENT,
   `target` varchar(50) DEFAULT NULL,
-  `emp_id` int(10) DEFAULT NULL,
+  `lg_id` int(10) DEFAULT NULL,
+  `achive` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`trid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `target` */
 
-insert  into `target`(`trid`,`target`,`emp_id`) values 
-(1,'1234',1);
+insert  into `target`(`trid`,`target`,`lg_id`,`achive`) values 
+(1,'120',12,'100'),
+(2,'3445',13,'455'),
+(3,'2001',14,'233');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
